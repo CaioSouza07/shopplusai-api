04dec93423153c171bf43656deca516de18dce67
Caio <caiodesouza.cds@gmail.com>
2026-04-25 12:21:35 -0300

    feat: cria endpoint de deletar no controller

--- src/main/java/com/univille/api/shopplusai/controller/UsuarioController.java
+++ src/main/java/com/univille/api/shopplusai/controller/UsuarioController.java
@@ -6,9 +6,7 @@ import com.univille.api.shopplusai.domain.usuario.dto.UsuarioResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
@@ -22,5 +20,11 @@ public class UsuarioController {
         var usuarioResponse = service.save(dados);
         var uri = uriBuilder.path("/{id}").buildAndExpand(usuarioResponse.id()).toUri();
         return ResponseEntity.created(uri).body(usuarioResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
