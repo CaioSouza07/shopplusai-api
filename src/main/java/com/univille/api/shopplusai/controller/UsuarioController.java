cc7ade11e54538ae45dc95fb84e878fccbd52b34
Caio <caiodesouza.cds@gmail.com>
2026-04-25 12:14:24 -0300

    feat: cria endpoint para cadastro de usuario

--- src/main/java/com/univille/api/shopplusai/controller/UsuarioController.java
+++ src/main/java/com/univille/api/shopplusai/controller/UsuarioController.java
@@ -1,8 +1,15 @@
 package com.univille.api.shopplusai.controller;
 
 import com.univille.api.shopplusai.domain.usuario.UsuarioService;
+import com.univille.api.shopplusai.domain.usuario.dto.CreateUsuarioRequest;
+import com.univille.api.shopplusai.domain.usuario.dto.UsuarioResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequiredArgsConstructor
@@ -10,5 +17,10 @@ public class UsuarioController {
 
     private final UsuarioService service;
 
-    
+    @PostMapping
+    public ResponseEntity<UsuarioResponse> save(@RequestBody CreateUsuarioRequest dados, UriComponentsBuilder uriBuilder){
+        var usuarioResponse = service.save(dados);
+        var uri = uriBuilder.path("/{id}").buildAndExpand(usuarioResponse.id()).toUri();
+        return ResponseEntity.created(uri).body(usuarioResponse);
+    }
 }
