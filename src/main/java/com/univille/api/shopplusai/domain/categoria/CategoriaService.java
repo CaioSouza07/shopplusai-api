1a7425d6ec8ef580c63faade8f649a76cb55d182
Caio <caiodesouza.cds@gmail.com>
2026-04-25 14:40:59 -0300

    feat: cria categoria service

---
+++ src/main/java/com/univille/api/shopplusai/domain/categoria/CategoriaService.java
@@ -0,0 +1,13 @@
+package com.univille.api.shopplusai.domain.categoria;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CategoriaService {
+
+    private final CategoriaRepository repository;
+
+    
+}
