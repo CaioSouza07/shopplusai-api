a0ed073cacbd1bb957e4135fe9c0b4b5112b91a0
Caio <caiodesouza.cds@gmail.com>
2026-04-25 10:51:13 -0300

    feat: cria entidade de usuario

--- src/main/java/com/univille/api/shopplusai/domain/usuario/Usuario.java
+++ src/main/java/com/univille/api/shopplusai/domain/usuario/Usuario.java
@@ -1,4 +1,25 @@
 package com.univille.api.shopplusai.domain.usuario;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "usuarios")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
 }
