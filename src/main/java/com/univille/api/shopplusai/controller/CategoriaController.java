d286894b6b09c7f3b14827380ec15a3f1db5f305
Caio <caiodesouza.cds@gmail.com>
2026-04-25 14:56:11 -0300

    feat: cria endpoint para cadastro de categoria

--- src/main/java/com/univille/api/shopplusai/controller/CategoriaController.java
+++ src/main/java/com/univille/api/shopplusai/controller/CategoriaController.java
@@ -1,9 +1,16 @@
 package com.univille.api.shopplusai.controller;
 
+import com.univille.api.shopplusai.domain.categoria.Categoria;
 import com.univille.api.shopplusai.domain.categoria.CategoriaService;
+import com.univille.api.shopplusai.domain.categoria.dto.CreateCategoriaRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/categoria")
@@ -11,5 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaController {
 
     private final CategoriaService service;
+
+    @PostMapping
+    public ResponseEntity<Categoria> save(@RequestBody @Valid CreateCategoriaRequest dados, UriComponentsBuilder uriBuilder){
+        var categoria = service.save(dados);
+        var uri = uriBuilder.path("/{id}").buildAndExpand(categoria.getId()).toUri();
+        return ResponseEntity.created(uri).body(categoria);
+    }
 
 }
